ca00d3380a6cf5b43e7ded6445f0dc6f87675baf
refathasan <refat.aiub@yahoo.com>
2016-09-03 17:48:47 +0600

    new class added by suvo

--- app/src/main/java/com/technodhaka/taxcalculator/MainActivity.java
+++ app/src/main/java/com/technodhaka/taxcalculator/MainActivity.java
@@ -1,17 +1,29 @@
 package com.technodhaka.taxcalculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     EditText edt_basic_sal, edt_house_rent,edt_conv,edt_medical,edt_fest_bonus;
-    TextView tex_total;
-    Button btnSum;
+    TextView text_total, text_Tax;
+    Button btnSum, btnClear;
+    Spinner spOperationSelection;
+    String strOperationMode;
+    Context mContext = MainActivity.this;
+    String TAG = MainActivity.class.getSimpleName();
+
+
+    CalTax ct;
 
    // String strBasicSal, strHouseRent, strConvene,strMedical,strFestBonus;
     int intBasicSal, intHouseRent, intConv, intMedical, intFestBonus, intTotal = 0;
@@ -21,16 +33,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewReference();
+
+
+
        // textChanged();
         //textConversion();
+
         btnSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textChanged();
-                tex_total.setText(String.valueOf(sumTotal()));
-                calculateTax();
+                text_total.setText(String.valueOf(sumTotal()));
+                taxCalculation();
+                ct = new CalTax(MainActivity.this);
+
+                ct.calculateTaxCom("av",null);
+                //calculateTax();
             }
         });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt_basic_sal.setText("");
+                edt_house_rent.setText("");
+                edt_conv.setText("");
+                edt_medical.setText("");
+                edt_fest_bonus.setText("");
+                text_total.setText("");
+                text_Tax.setText("");
+            }
+        });
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mContext,R.array.operation_Mode,R.layout.spinner_layout);
+        spOperationSelection.setAdapter(adapter);
+        if(strOperationMode!=null)
+        {
+            spOperationSelection.setSelection(getSpinnerIndex(spOperationSelection, strOperationMode));
+        }
+        spOperationSelection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                strOperationMode = parent.getItemAtPosition(position).toString();
+
+                if(strOperationMode.equals("Male"))
+                {
+                    //TODO:
+                }
+                else if(strOperationMode.equals("Female"))
+                {
+                    //TODO:
+                }
+                else if(strOperationMode.equals("65Y+Male"))
+                {
+                    //TODO:
+                }
+                else if(strOperationMode.equals("65Y+Female"))
+                {
+                    //TODO:
+                }
+                else if(strOperationMode.equals("Business"))
+                {
+                    //TODO:
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
 
@@ -41,8 +112,11 @@ public class MainActivity extends AppCompatActivity {
         edt_conv =(EditText)findViewById(R.id.edt_convence);
         edt_medical =(EditText)findViewById(R.id.edt_medical);
         edt_fest_bonus =(EditText)findViewById(R.id.edt_fest_bonus);
-        tex_total =(TextView) findViewById(R.id.tex_total_sal);
+        text_total =(TextView) findViewById(R.id.text_total_sal);
+        text_Tax =(TextView)findViewById(R.id.text_total_tax);
         btnSum = (Button)findViewById(R.id.btnCount);
+        btnClear =(Button)findViewById(R.id.btnClear);
+        spOperationSelection =(Spinner)findViewById(R.id.spOperationOption);
     }
     
     private void textChanged()
@@ -94,17 +168,80 @@ public class MainActivity extends AppCompatActivity {
     private Integer sumTotal()
     {
         intTotal = (intBasicSal + intHouseRent + intConv + intMedical + intFestBonus);
-        return intTotal;
+        return intTotal*12;
     }
 
-    private void calculateTax()
+    private int calculateBaseSal(int intBasicSal)
     {
-        int tax;
-        if(intBasicSal<=2200 && intBasicSal>300000) {
-            tax = (intBasicSal * 0) / 100;
-            Toast.makeText(MainActivity.this,"TAX is: "+tax, Toast.LENGTH_SHORT).show();
+        int baseSalary = 0;
+        baseSalary = intBasicSal*12;
+        return baseSalary;
+    }
+
+    private void taxCalculation()
+    {
+
+        double tax  = 0;
+        int index   = calculateBaseSal(intBasicSal) ;
+        int onFirst = 220000;
+        int next1   = 300000 ;
+        int next2   = 400000 ;
+        int next3   = 500000;
+        int next4   = 3000000 ;
+        int rest    = 3000001;
+
+        if(index >0 && index<onFirst)
+        {
+            Toast.makeText(MainActivity.this, "TAX: Nil", Toast.LENGTH_SHORT).show();
+            text_Tax.setText("Nil");
+        }
+        else if(index>=onFirst && index<next1)
+        {
+            Toast.makeText(MainActivity.this, "TAX: Nil", Toast.LENGTH_SHORT).show();
+            text_Tax.setText("Nil");
+        }
+        else if(index>=next1 && index<next2)
+        {
+            tax = (double)((index*10)/100);
+            Toast.makeText(MainActivity.this, "TAX:"+tax, Toast.LENGTH_SHORT).show();
+            text_Tax.setText(String.valueOf(tax));
+        }
+        else if(index>=next2 && index<next3)
+        {
+            tax = (double)((index*15)/100);
+            Toast.makeText(MainActivity.this, "TAX:"+tax, Toast.LENGTH_SHORT).show();
+            text_Tax.setText(String.valueOf(tax));
+        }
+        else if(index >= next3 && index<next4)
+        {
+            tax = (double)((index*20)/100);
+            Toast.makeText(MainActivity.this, "TAX:"+tax, Toast.LENGTH_SHORT).show();
+            text_Tax.setText(String.valueOf(tax));
+        }
+        else if(index>=next4 && index<rest )
+        {
+            tax = (double)((index*25)/100);
+            Toast.makeText(MainActivity.this, "TAX:"+tax, Toast.LENGTH_SHORT).show();
+            text_Tax.setText(String.valueOf(tax));
+        }
+        else if(index>=rest )
+        {
+            tax = (double)((index*30)/100);
+            Toast.makeText(MainActivity.this, "TAX:"+tax, Toast.LENGTH_SHORT).show();
+            text_Tax.setText(String.valueOf(tax));
         }
     }
 
+    protected int getSpinnerIndex(Spinner spinner, String myString) {
+
+        int index = 0;
+
+        for (int i = 0; i < spinner.getCount(); i++) {
+            if (spinner.getItemAtPosition(i).equals(myString)) {
+                index = i;
+            }
+        }
+        return index;
+    }
 
 }
--- app/src/main/java/com/technodhaka/taxcalculator/SplashScreenActivity.java
+++ app/src/main/java/com/technodhaka/taxcalculator/SplashScreenActivity.java
@@ -1,12 +1,13 @@
 package com.technodhaka.taxcalculator;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
     private Context mContext = SplashScreenActivity.this;
     private static final int DISPLAY_TIMEOUT = 4000;
 
